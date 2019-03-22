07e339d39e15f98ffeb080d3990ed2d979bcc7c4
Rafkat <ufadiz@gmail.com>
2019-03-22 12:54:32 +0300

    Initial

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloWorld greetings="Где деньги, Лебовски?"/>
+  </div>
+</template>
+
+<script>
+import Chat from './components/Chat.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ web/src/components/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
