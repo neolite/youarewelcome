83343dce2a952d6d4a7f71ba505ced21a8c2dc4c
Rafkat <ufadiz@gmail.com>
2019-03-22 15:23:02 +0300

    Main logic

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,19 +1,54 @@
 <template>
   <div id="app">
-    <HelloWorld greetings="Где деньги, Лебовски?"/>
+    <Chat
+      greetings="..."
+      :messages="this.messages"
+      :send-message="handleSendMessage"
+    />
   </div>
 </template>
 
 <script>
-import Chat from './components/Chat.vue'
+import Chat from "./components/Chat.vue";
+
+const api = async (url, msg) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  const bodyParams = new URLSearchParams();
+  bodyParams.append("q", msg);
+  const response = await fetch("/api/get-answer", {
+    method: "post",
+    headers: myHeaders,
+    body: bodyParams
+  });
+  return response.json();
+};
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Chat
+  },
+  data: function() {
+    return {
+      messages: [
+        {
+          remote: true,
+          text: "Где деньги, Лебовски?"
+        }
+      ]
+    };
+  },
+  methods: {
+    async handleSendMessage(msg) {
+      this.messages.push({ remote: false, text: msg });
+      const resp = await api("/api/get-answer", msg);
+      if (resp.ok) {
+        this.messages.push({ remote: true, text: resp.a });
+      } else {
+        console.log("ERR:", resp.error);
+      }
+    }
   }
-}
+};
 </script>
-
-<style>
-</style>
--- web/src/components/Chat.vue
+++ web/src/components/Chat.vue
@@ -1,15 +1,57 @@
 <template>
+  <div class="messenger-wrapper">
+    <div class="messenger">
+      <span class="chat-header">
+        <span class="chat-title">
+          Messenger
+        </span>
+      </span>
+      <div id="chat" class="chat-window">
+        <ul class="chat-container">
+          <li
+            v-for="(msg, idx) in this.messages"
+            v-bind:key="idx"
+            class="chat-item"
+            v-bind:class="{ 'from-server': msg.remote }"
+          >
+            {{ msg.text }}
+          </li>
+        </ul>
+      </div>
+      <div class="message-input">
+        <input type="text" v-model="message"  v-on:keyup.enter="handleMessage" :placeholder="this.greetings"/>
+        <button @click="handleMessage" class="send-button">
+          Send
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Chat',
   props: {
-    msg: String
+    greetings: String,
+    messages: Array,
+    sendMessage: Function,
+  },
+  data: function () {
+      return {
+          message: ""
+      }
+  },
+  mounted() {
+
+  },
+  methods:  {
+      handleMessage() {
+        this.sendMessage(this.message)
+        this.message = ""
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style lang="scss" src="./styles.scss"></style>
